efadad476055f92c2e68b4b91bdf9e5a226f6ecf
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-21 15:14:59 +0200

    Create DispositivoRepository.java

---
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/repositories/DispositivoRepository.java
@@ -0,0 +1,11 @@
+package com.example.Unit02Week03Day01.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.Unit02Week03Day01.entities.Dispositivo;
+
+@Repository
+public interface DispositivoRepository extends JpaRepository<Dispositivo, Long> {
+
+}
