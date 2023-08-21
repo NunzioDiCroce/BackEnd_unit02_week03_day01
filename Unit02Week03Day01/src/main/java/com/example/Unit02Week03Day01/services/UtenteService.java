44f6db0404d323cb94212b2372a2e448672c300b
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-21 15:20:05 +0200

    Create UtenteService.java

---
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/services/UtenteService.java
@@ -0,0 +1,11 @@
+package com.example.Unit02Week03Day01.services;
+
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class UtenteService {
+
+}
