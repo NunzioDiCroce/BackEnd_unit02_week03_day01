dbf42395040b8bb6482e3d4569c67ef70fc2f6f6
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-21 15:29:40 +0200

    Create UtentePayload.java

---
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/entities/UtentePayload.java
@@ -0,0 +1,17 @@
+package com.example.Unit02Week03Day01.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UtentePayload {
+
+	protected String userName;
+	protected String nome;
+	protected String cognome;
+	protected String mail;
+
+}
