cdbcc60294d943eef953376406f05076d57dee10
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-21 15:04:49 +0200

    Create Dispositivo.java

--- Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/entities/Dispositivo.java
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/entities/Dispositivo.java
@@ -1,5 +1,42 @@
 package com.example.Unit02Week03Day01.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Table(name = "dispositivi")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Slf4j
+@Builder
 public class Dispositivo {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	protected String tipo;
+	protected String stato;
+
+	@ManyToOne
+	protected Utente utente;
+
+	public Dispositivo(String tipo, String stato) {
+		this.tipo = tipo;
+		this.stato = stato;
+	}
 
 }
