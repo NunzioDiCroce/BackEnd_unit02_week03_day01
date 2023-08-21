751cf84df533106a48f1b852b00e1a0abf002791
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-22 01:52:47 +0200

    Create NewUserResponsePayload.java

---
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/payloads/NewUserResponsePayload.java
@@ -0,0 +1,14 @@
+package com.example.Unit02Week03Day01.payloads;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class NewUserResponsePayload {
+
+	private UUID id;
+
+}
