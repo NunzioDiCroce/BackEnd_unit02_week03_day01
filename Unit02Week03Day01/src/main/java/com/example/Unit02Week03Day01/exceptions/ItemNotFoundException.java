b690ed9bb254c5f8218592a8852fc20a6805d5bb
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-21 16:46:44 +0200

    Update ItemNotFoundException.java

--- Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/exceptions/ItemNotFoundException.java
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/exceptions/ItemNotFoundException.java
@@ -1,8 +1,10 @@
 package com.example.Unit02Week03Day01.exceptions;
 
+import java.util.UUID;
+
 public class ItemNotFoundException extends RuntimeException {
 
-	public ItemNotFoundException(long _id) {
+	public ItemNotFoundException(UUID _id) {
 		super("Item with id " + _id + " not found!");
 	}
 
