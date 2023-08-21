95e42a358a95b3885fa81597114755520b0df92f
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-21 15:24:40 +0200

    Update UtenteService.java

--- Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/services/UtenteService.java
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/services/UtenteService.java
@@ -1,11 +1,62 @@
 package com.example.Unit02Week03Day01.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.Unit02Week03Day01.entities.Utente;
+import com.example.Unit02Week03Day01.repositories.UtenteRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class UtenteService {
+
+	private final UtenteRepository utenteRepository;
+
+	@Autowired
+	public UtenteService(UtenteRepository utenteRepository) {
+		this.utenteRepository = utenteRepository;
+	}
+
+	// save utente
+	public void save(Utente utente) {
+		utenteRepository.save(utente);
+		log.info("Utente con ID " + utente.getId() + " salvato con successo");
+
+	}
+
+	// save by UtentePayload
+	public Utente save(UtentePayload body) {
+		Utente nuovoUtente = new Utente(body.getUserName(), body.getNome(), body.getCognome(), body.getMail());
+		return utenteRepository.save(nuovoUtente);
+	}
+
+	public List<Utente> findAll() {
+		return utenteRepository.findAll();
+	}
+
+	public Utente findById(long id) throws ItemNotFoundException {
+		return utenteRepository.findById(id).orElseThrow(() -> new ItemNotFoundException(id));
+
+	}
+
+	public Utente findByIdAndUpdate(long id, UtentePayload body) throws ItemNotFoundException {
+		Utente found = this.findById(id);
+
+		found.setUserName(body.getUserName());
+		found.setNome(body.getNome());
+		found.setCognome(body.getCognome());
+		found.setMail(body.getMail());
+
+		return utenteRepository.save(found);
+	}
+
+	public void findByIdAndDelete(long id) throws ItemNotFoundException {
+		Utente found = this.findById(id);
+		utenteRepository.delete(found);
+	}
 
 }
