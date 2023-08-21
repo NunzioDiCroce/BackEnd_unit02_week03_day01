88672553ae422f7a57e100533959b9a69deb3779
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-21 14:40:47 +0200

    Create Spring Boot application

---
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/Unit02Week03Day01Application.java
@@ -0,0 +1,13 @@
+package com.example.Unit02Week03Day01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit02Week03Day01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit02Week03Day01Application.class, args);
+	}
+
+}
