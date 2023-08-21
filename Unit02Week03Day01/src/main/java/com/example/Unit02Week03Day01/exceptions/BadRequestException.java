7c59ee2c5d899c1178c65b52ffb6784ad59ee58f
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-21 18:33:42 +0200

    Update BadRequestException.java

--- Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/exceptions/BadRequestException.java
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/exceptions/BadRequestException.java
@@ -1,5 +1,9 @@
 package com.example.Unit02Week03Day01.exceptions;
 
-public class BadRequestException {
+public class BadRequestException extends RuntimeException {
+
+	public BadRequestException(String message) {
+		super(message);
+	}
 
 }
