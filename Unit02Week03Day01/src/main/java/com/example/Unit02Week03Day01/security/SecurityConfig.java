4119ba0ed697d1663b2544e4e420f4c1e53506b4
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-21 16:19:17 +0200

    Create JWTAuthFilter.java

--- Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/security/SecurityConfig.java
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/security/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.example.Unit02Week03Day01.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
 }
