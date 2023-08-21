ea4dd163d7d048a7686ac9b58097671c65d17a3e
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-21 18:24:48 +0200

    Update UserRequestPayload.java

--- Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/payloads/UserRequestPayload.java
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/payloads/UserRequestPayload.java
@@ -1,5 +1,15 @@
 package com.example.Unit02Week03Day01.payloads;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserRequestPayload {
+
+	private String nome;
+	private String cognome;
+	private String mail;
+	private String password;
 
 }
