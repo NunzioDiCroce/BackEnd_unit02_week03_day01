716c23d35f03eac2e2d3af18804ba955d7723f9b
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-21 18:46:55 +0200

    Update NotFoundException.java

--- Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/exceptions/NotFoundException.java
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/exceptions/NotFoundException.java
@@ -1,5 +1,15 @@
 package com.example.Unit02Week03Day01.exceptions;
 
-public class NotFoundException {
+import java.util.UUID;
+
+public class NotFoundException extends RuntimeException {
+
+	public NotFoundException(String message) {
+		super(message);
+	}
+
+	public NotFoundException(UUID id) {
+		super(id + " non trovato!");
+	}
 
 }
