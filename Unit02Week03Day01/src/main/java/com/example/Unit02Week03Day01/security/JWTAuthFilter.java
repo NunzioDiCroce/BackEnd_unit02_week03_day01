cff4d1302507afcfde50c1ea4a8ab4779cf53434
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-21 16:40:29 +0200

    Update JWTAuthFilter.java

--- Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/security/JWTAuthFilter.java
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/security/JWTAuthFilter.java
@@ -1,5 +1,58 @@
 package com.example.Unit02Week03Day01.security;
 
-public class JWTAuthFilter {
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.Unit02Week03Day01.exceptions.UnauthorizedException;
+import com.example.Unit02Week03Day01.services.UtenteService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JWTAuthFilter extends OncePerRequestFilter {
+
+	@Autowired
+	JWTTools jwttools;
+	@Autowired
+	UtenteService utenteService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		String authHeader = request.getHeader("Authorization");
+
+		if (authHeader == null || !authHeader.startsWith("Bearer "))
+			throw new UnauthorizedException("Per favore passa il token nell'authorization header");
+		String token = authHeader.substring(7);
+		System.out.println("TOKEN -------> " + token);
+
+		jwttools.verifyToken(token);
+
+		String id = jwttools.extractSubject(token);
+		User currentUser = usersService.findById(UUID.fromString(id));
+
+		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(currentUser, null,
+				currentUser.getAuthorities());
+
+		SecurityContextHolder.getContext().setAuthentication(authToken);
+
+		filterChain.doFilter(request, response);
+
+	}
+
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest request) {
+		System.out.println(request.getServletPath());
+
+		return new AntPathMatcher().match("/auth/**", request.getServletPath());
+	}
 
 }
