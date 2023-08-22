b09bedb93afaa0276c26137acc53b6004166546e
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-22 12:18:18 +0200

    Update NewUserResponsePayload.java

--- Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/payloads/NewUserResponsePayload.java
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/payloads/NewUserResponsePayload.java
@@ -3,8 +3,12 @@ package com.example.Unit02Week03Day01.payloads;
 import java.util.UUID;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
+@Getter
+@Setter
 @AllArgsConstructor
 @ToString
 public class NewUserResponsePayload {
