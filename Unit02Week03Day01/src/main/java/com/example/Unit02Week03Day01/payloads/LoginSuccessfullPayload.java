b2a5ad8342f63bb248f8a0dfd352021dc71607a2
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-22 12:19:15 +0200

    Update LoginSuccessfullPayload.java

--- Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/payloads/LoginSuccessfullPayload.java
+++ Unit02Week03Day01/src/main/java/com/example/Unit02Week03Day01/payloads/LoginSuccessfullPayload.java
@@ -1,7 +1,11 @@
 package com.example.Unit02Week03Day01.payloads;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @AllArgsConstructor
 public class LoginSuccessfullPayload {
 
